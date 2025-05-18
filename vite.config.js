c0642fb65f62c8e6334019e27482e7894374ea1c
Johne Young <your@email.com>
2025-05-18 12:55:59 -0500

    Fix main.js MIME + add manifest/favicon

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  publicDir: 'public'
+})
